71379686515839b41ba27ea733f5f35acb431b3d
HUGO MUÑOZ <hugomo0897@gmail.com>
2025-02-19 16:14:36 -0600

    se agrega el modulo de revision de checklists

--- app/Http/Controllers/IndicatorController.php
+++ app/Http/Controllers/IndicatorController.php
@@ -521,7 +521,7 @@ class IndicatorController extends Controller
             $pregunta['files']= $filesWithUrls;
             }
         }
-        $users = User::with('rol.area')->where('_state','!=',4)->get();
+        $users = User::with('rol.area')->get();
         $res = [
             "responses"=>$response,
             "usuarios"=>$users
