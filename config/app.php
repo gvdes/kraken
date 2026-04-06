485d6925f0df7d2b2e9a2c5aa2fd6c232262fa2f
HUGO MUÑOZ <hugomo0897@gmail.com>
2026-04-06 09:10:02 -0600

    se hace el cambio de el horario

--- config/app.php
+++ config/app.php
@@ -69,8 +69,8 @@ return [
     |
     */
 
-    'timezone' => 'America/Mexico_City',
-    // 'timezone' => 'America/Mazatlan',
+    // 'timezone' => 'America/Mexico_City',
+    'timezone' => 'America/Mazatlan',
 
 
     /*
